8b13b696e2de70e5ff7533aef908b37025c894b2
ElenaGil <elenagillich@web.de>
2025-08-29 14:40:48 +0200

    Array Uebungen sind durchgemacht

--- src/Main.java
+++ src/Main.java
@@ -1,364 +1,121 @@
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.logging.Level;
 
 public class Main {
     public static void main(String[] args) {
 
-// MAIN_METHOD_EXERCISES
-// ============================================================================
-        System.out.println("Hello World");
-        System.out.println("Test");
-        System.out.println("Test2");
-
-        String myName = "Elena";
-        System.out.println("Hello, " + myName);
-
-        System.out.println(3+5);
-        System.out.println(4*4);
-        System.out.println("10-7=" + (10-7));
-        int a = 8;
-        int b = 2;
-        System.out.println(a / b);
-
-        // Aufgabe 8
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wie heißen Sie?");
-        String userName = scanner.nextLine();
-        System.out.println("Ich begrüße Sie, " + userName + "!");
+// ARRAY_EXERCISES
+// ============================================================================
+
+        // Level 1
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < numbers.length ; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        //------------------------
+        String[] names = { "Anna", "Max", "Peter", "Monika" };
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        //------------------------
+        int[] nums = { 2, 4, 6, 8 };
+        for (int i = 0; i < nums.length ; i++) {
+            System.out.println(Math.pow(nums[i], 2));
+        }
+
+        // Level 2
+        int[] tenNums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int i = 0; i < tenNums.length ; i++) {
+            System.out.print(tenNums[i] % 2 == 0 ? tenNums[i] : " ");
+        }
+        System.out.println();
+        for (int num : tenNums) {
+            System.out.print(num % 2 == 0 ? num : " ");
+        }
         System.out.println();
 
-        // Aufgabe 9
-        System.out.println("========================");
-        System.out.println("| Peter         |  18  |");
-        System.out.println("| Mirabella     |  21  |");
-        System.out.println("| Jonny         |  15  |");
-        System.out.println("========================");
+        //------------------------
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Die Summe:" + sum);
 
-        // Aufgabe 10
-
-        System.out.println("Mein Name ist Elena");
-        System.out.println("Mir ist 45");
-        System.out.println("Mein Lieblingsspruch: Das Leben ist wunderbar!");
-        System.out.println(45 * 2);
-
-        System.out.println("_______________________________");
-
-
-// DATA_TYPES_EXERCISES
-// ============================================================================
-
-        // Level 1
-        int age = 45;
-        System.out.println(age);
-
-        double height = 1.68;
-        System.out.println(height);
-
-        char firstLetter = 'E';
-        System.out.println(firstLetter);
-
-        String firstName = "Elena";
-        System.out.println(firstName);
-
-        // Level2
-
-        System.out.println("Mein Name ist " + firstName + ", ich bin " + age + " Jahre alt und " + height + " Meter groß.");
-
-        boolean isAdult = age >= 18;
-
-        if (isAdult) {
-            System.out.println("Ich bin erwachsen.");
+        //------------------------
+        String[] cities = { "Berlin", "Hamburg", "Uelze", "Moskau" };
+        System.out.println();
+        for (int i = 0; i < cities.length; i++) {
+            System.out.println(i+1 + ". " + cities[i]);
         }
 
-        System.out.println("___________________________");
-        int x = 25;
-        int y = 5;
-        System.out.println("Summe von " + x + " und " + y +  " ist " + (x+y));
-        System.out.println("Differenz von " + x + " und " + y +  " ist " + (x-y));
-        System.out.println("Produkt von " + x + " und " + y +  " ist " + (x*y));
-        System.out.println("Quotient von " + x + " und " + y +  " ist " + (x/y));
-
-        System.out.println("___________________________");
-
-        String satz = "Lorem ipsum dolores";
-        System.out.println(satz);
-        System.out.println(satz.length());
-        System.out.println(satz.charAt(0));
-        System.out.println(satz.toUpperCase());
-
-        System.out.println("___________________________");
+        //------------------------
+        int[] numArr = { 12, 24, 3, 48, 5, 60, 71, 18, 9, 10 };
+        int min = numArr[0];
+        int max = numArr[0];
+        for (int num : numArr) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
 
         // Level 3
-        System.out.println("Wie heißt du?");
-        String userFirstName = scanner.nextLine();
-
-        System.out.println("Wie alt bis du?");
-        int userAge = scanner.nextInt();
-
-        System.out.println("Wie ist deine Lieblingsbuchstabe?");
-        char userFavorLetter = scanner.next().charAt(0);
-
-        System.out.println("Hallo " + userFirstName + ", dein Lieblingsbuchstabe ist " + userFavorLetter + " und du bist " + userAge + " Jahre alt.");
-
-        double d = 1.23;
-        double f = 4.56;
-
-        if (d+f > 10) {
-            System.out.println("Summe ist größer als 10.");
-        } else {
-            System.out.println("Summe ist kleiner als 10.");
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = 2 * numArr[i];
+            System.out.println(numArr[i]);
         }
 
-        System.out.println("___________________________");
+        //------------------------
+        String[] lastNames = { "Schmidt", "Mustermann", "Hase", "Schreibikus" };
 
-        System.out.println("Füg bitte einen Zeichen ein: ");
-        char zeichen = scanner.next().charAt(0);
-        char[] vocalArr = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
-
-        boolean isVocal = false;
-
-        for (char vocal : vocalArr) {
-            if (vocal == zeichen) {
-                isVocal = true;
-                break;
-            }
+        System.out.println("_NAMES_:");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + " " + lastNames[i]);
         }
 
-        if (isVocal) {
-            System.out.println("Vokal!");
-        } else {
-            System.out.println("Kein Vokal!");
+        //------------------------
+        int[] threeNums = new int[3];
+        System.out.println("---- Nenne 3 Ganzzahlen ----");
+
+        for (int i = 1; i <= threeNums.length; i++) {
+            System.out.println("Nenne Zahl " + i + ":");
+            threeNums[i-1] = scanner.nextInt();
+        }
+        System.out.print("Du hast folgende Zahlen eingegeben: ");
+        for (int num : threeNums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        //------------------------ Notenrechner
+        int[] scors = { 85, 70, 15, 60 };
+        int summe = 0;
+        for (int score : scors) {
+            summe += score;
+        }
+        int average = summe / scors.length;
+        System.out.println("Die Durchschnitt ist: " + average);
+        System.out.println(average > 70 ? "Bestanden!" : "Nicht bestanden!");
+
+        //------------------------ Zufallszahlen sortieren
+
+        int[] randomNums = new int[10];
+        for (int i = 0; i < randomNums.length; i++) {
+            randomNums[i] = (int) (Math.random() * 100);
+        }
+        System.out.println("--- Unsortiert ---");
+        for (int num : randomNums) {
+            System.out.print(num + " ");
         }
 
-        System.out.println("___________________________");
-
-        System.out.println("Nenne erste Ganzzahl: ");
-        int firstNum = scanner.nextInt();
-
-        System.out.println("Nenne zweite Ganzzahl: ");
-        int secondNum = scanner.nextInt();
-
-        System.out.println("Nenne eine Kommazahl: ");
-        double doubleNum = scanner.nextDouble();
-
-        double result = (double)(firstNum + secondNum) - doubleNum;
-        System.out.println(firstNum + " + " + secondNum + " - " + doubleNum + " = " + result);
-
-        if (result > 0) {
-            System.out.println("Das Ergebnis ist positiv!");
-        } else  {
-            System.out.println("Das Ergebnis ist negativ!");
-        }
-
-// OPERATOREN_EXERCISES
-// ============================================================================
-        // Level 1
-        int num1 = 12;
-        int num2 = 8;
-        int sum = num1 + num2;
-        System.out.println("Summe: " + sum);
-
-        if (num1 == num2) {
-            System.out.println("Die Zahlen sind gleich!");
-        } else {
-            System.out.println("Die Zahlen sind ungleich!");
-        }
-
-        System.out.println(num1 > 0 && num1 < 100);
-
-
-        // Level 2
-        int rest = num1 % num2;
-        System.out.println(rest == 0 && num1 > 0 && num2 > 0);
-
-        double preis = 15.75;
-        boolean rabatt = true;
-
-        if (rabatt == true) {
-            preis = preis - preis * 10 / 100;
-        }
-        System.out.println("Preis: " + preis);
-
-        // Prüfen, ob eine Zahl entweder gerade oder größer als 50 ist.
-        int num = 77;
-        if (num % 2 == 0 || num > 50) {
-            System.out.println("Die Zahl ist gerade oder größer als 50");
-        }
-
-        // Level 3 - Bonus
-        // -//-
-
-// IF_ELSE_EXERCISE
-// ============================================================================
-
-        // Level 1
-        int age1 = 23;
-        if (age1 >= 18) {
-            System.out.println("Volljährig!");
-        }
-
-        boolean isRaining = true;
-        if (isRaining) {
-            System.out.println("Nimm einen Regenschirm!");
-        } else {
-            System.out.println("Kein Regenschirm nötig");
-        }
-
-        String city = "Berlin";
-        if (city.contains("Berlin")) {
-            System.out.println("Hauptstadt!");
-        }
-
-        // Level 2
-        int score = 22;
-        boolean hasTicket = true;
-        if (hasTicket && score >= 50) {
-            System.out.println("Eintritt erlaubt!");
-        } else {
-            System.out.println("Eintritt nicht erlaubt!");
-        }
-
-        // ------------------
-        int temperature = 37;
-
-        if (temperature >= 30) {
-            System.out.println("Heiß!");
-        } else if (temperature >= 20) {
-            System.out.println("Warm");
-        } else if (temperature >= 10) {
-            System.out.println("Kühl");
-        } else {
-            System.out.println("Kalt");
-        }
-
-        // ------------------
-
-        int number = 19;
-        String parity = number % 2 == 0 ? "Gerade" : "Ungerade";
-
-        // Level 3
-
-        for (int i = 0; i < 3; i++) {
-            System.out.print("----- Wie ist Ihr Name? -----");
-            String name = scanner.nextLine();
-
-            if (name.trim().isEmpty()) {
-                System.out.println("Ungültiger Name!");
-            } else {
-                System.out.println("Hallo, " + name);
-            }
-        }
-
-        // ------------------
-
-        double xx = 0.1 + 0.2;
-        System.out.println("Die Summe von zwei doubles: " + xx);
-        String yy = xx + "";
-
-        if (yy.contains("0.3")) {
-            System.out.println("Fast 0.3");
-        } else {
-            System.out.println("Nicht gleich 0.3");
-        }
-
-        // ------------------
-
-        register("Tomas", 17);
-        register("Milan", 21);
-
-// METHODEN_EXERCISE
-// ============================================================================
-
-        // Level 1
-        printHello();
-        getGreeting();
-        printName("Monika");
-
-        // Level 2
-        int summe = add(5, 7);
-        System.out.println("5 + 7" + summe);
-
-        System.out.println("Calculated area: " + calculateArea(2.5, 3.5));
-
-        checkPositive(4);
-        checkPositive(-5);
-
-        // Level 3
-        System.out.println("Ist die '13' gerade? " + isEven(13));
-
-        printSumMessage(20, 30);
-
-        // Calculation
-        add(10, 1);
-        add(10, -1);
-        subtract(-12, -5);
-        subtract(12, -5);
-        subtract(0, 3);
-        multiply(0, 0);
-        multiply(-3, 7);
-        divide(20.5,5);
-        divide(4.15, 0);
-        divide(4.15, 0.15);
-        divide(3.3, -33.3);
-    }
-
-    private static void printSumMessage(int x, int y) {
-        System.out.println("Die Summe von " + x + " und " + y + " ist " + add(x, y));
-    }
-
-
-    private static boolean isEven(int num) {
-        return num % 2 == 0;
-    }
-
-    private static void checkPositive(int number) {
-        if (number < 0) {
-            System.out.println("Negative Zahl!");
-            return;
-        }
-    }
-    private static double calculateArea(double width, double height) {
-        return width * height;
-    }
-
-    private static int add(int a, int b) {
-        System.out.println(a + b);
-        return a + b;
-    }
-    private static int subtract(int a, int b) {
-        System.out.println(a - b);
-        return a - b;
-    }
-    private static int multiply(int a, int b) {
-        System.out.println(a * b);
-        return a * b;
-    }
-    private static double divide(double a, double b) {
-        if (b == 0d) {
-            System.out.println("Division durch 0!");
-            return 0d;
-        }
-        System.out.println(a / b);
-        return a / b;
-    }
-
-    private static void printName(String name) {
-        System.out.println("Hallo " + name);
-    }
-
-    private static void printHello() {
-        System.out.println("Hallo Welt!");
-    }
-
-    private static String getGreeting() {
-        return "Willkommen im Java-Kurs";
-    }
-
-    private static void register(String username, int age) {
-        if (username == null || username.isEmpty() || age < 18) {
-            return;
-        } else {
-            System.out.println(username + ": Registrierung erfolgreich");
+        Arrays.sort(randomNums);
+        System.out.println();
+        System.out.println("--- Sortiert ---");
+        for (int num : randomNums) {
+            System.out.print(num + " ");
         }
     }
 }
